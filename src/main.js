5fcab774243f0fa796722ce8599b59a5bce0b7a2
Key <springfield0329@gmail.com>
2023-01-14 00:13:31 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// import './style.css'
+import './assets/all.scss'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueAxios, axios)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1 class="bg-primary">Hello, This is Home Page.</h1>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <hr />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
